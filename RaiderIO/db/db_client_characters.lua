1774af61fdc67516cf90cc2aeb7f655cfe8abe19
Devin Webster <TritanIV@gmail.com>
2019-08-13 21:55:52 -0700

    details, raiderio, and rarescanner updates

--- RaiderIO/db/db_client_characters.lua
+++ RaiderIO/db/db_client_characters.lua
@@ -1,5 +1,5 @@
 --
--- RaiderIO Client Data. Generated on 2019-08-09T07:55:51Z
+-- RaiderIO Client Data. Generated on 2019-08-14T04:39:11Z
 --
 local _, ns = ...
 ns.CLIENT_CHARACTERS = {
@@ -13,35 +13,35 @@ ns.CLIENT_CHARACTERS = {
     },
     ["mythic_keystone"] = {
       ["all"] = {
-        ["score"] = 558.6,
+        ["score"] = 671.2,
         ["best"] = {
-          ["zone_id"] = 9526,
+          ["zone_id"] = 9391,
           ["level"] = 9,
           ["upgrades"] = 0,
-          ["fraction"] = 1.094,
-          ["url"] = "https://raider.io/mythic-plus-runs/season-bfa-3/2817176-9-kings-rest?utm_source=client"
+          ["fraction"] = 1.028,
+          ["url"] = "https://raider.io/mythic-plus-runs/season-bfa-3/3202209-9-the-underrot?utm_source=client"
         },
         ["runs"] = {
           [1] = {
             ["zone_id"] = 8064,
             ["level"] = 8,
             ["upgrades"] = 1,
-            ["fraction"] = 0.938,
-            ["url"] = "https://raider.io/mythic-plus-runs/season-bfa-3/2626123-8-the-motherlode?utm_source=client"
+            ["fraction"] = 0.932,
+            ["url"] = "https://raider.io/mythic-plus-runs/season-bfa-3/3228093-8-the-motherlode?utm_source=client"
           },
           [2] = {
             ["zone_id"] = 9028,
-            ["level"] = 0,
+            ["level"] = 9,
             ["upgrades"] = 0,
-            ["fraction"] = 0,
-            ["url"] = nil
+            ["fraction"] = 1.301,
+            ["url"] = "https://raider.io/mythic-plus-runs/season-bfa-3/3226846-9-ataldazar?utm_source=client"
           },
           [3] = {
             ["zone_id"] = 9164,
             ["level"] = 8,
-            ["upgrades"] = 1,
-            ["fraction"] = 0.931,
-            ["url"] = "https://raider.io/mythic-plus-runs/season-bfa-3/2815373-8-freehold?utm_source=client"
+            ["upgrades"] = 2,
+            ["fraction"] = 0.74,
+            ["url"] = "https://raider.io/mythic-plus-runs/season-bfa-3/3175218-8-freehold?utm_source=client"
           },
           [4] = {
             ["zone_id"] = 9327,
@@ -59,10 +59,10 @@ ns.CLIENT_CHARACTERS = {
           },
           [6] = {
             ["zone_id"] = 9391,
-            ["level"] = 5,
-            ["upgrades"] = 2,
-            ["fraction"] = 0.799,
-            ["url"] = "https://raider.io/mythic-plus-runs/season-bfa-3/1656793-5-the-underrot?utm_source=client"
+            ["level"] = 9,
+            ["upgrades"] = 0,
+            ["fraction"] = 1.028,
+            ["url"] = "https://raider.io/mythic-plus-runs/season-bfa-3/3202209-9-the-underrot?utm_source=client"
           },
           [7] = {
             ["zone_id"] = 9424,
@@ -73,10 +73,10 @@ ns.CLIENT_CHARACTERS = {
           },
           [8] = {
             ["zone_id"] = 9525,
-            ["level"] = 8,
+            ["level"] = 9,
             ["upgrades"] = 1,
-            ["fraction"] = 0.815,
-            ["url"] = "https://raider.io/mythic-plus-runs/season-bfa-3/2809860-8-shrine-of-the-storm?utm_source=client"
+            ["fraction"] = 0.995,
+            ["url"] = "https://raider.io/mythic-plus-runs/season-bfa-3/2997069-9-shrine-of-the-storm?utm_source=client"
           },
           [9] = {
             ["zone_id"] = 9526,
@@ -87,10 +87,10 @@ ns.CLIENT_CHARACTERS = {
           },
           [10] = {
             ["zone_id"] = 9527,
-            ["level"] = 7,
-            ["upgrades"] = 2,
-            ["fraction"] = 0.769,
-            ["url"] = "https://raider.io/mythic-plus-runs/season-bfa-3/1790348-7-temple-of-sethraliss?utm_source=client"
+            ["level"] = 8,
+            ["upgrades"] = 1,
+            ["fraction"] = 0.908,
+            ["url"] = "https://raider.io/mythic-plus-runs/season-bfa-3/3208310-8-temple-of-sethraliss?utm_source=client"
           }
         }
       }
--- RaiderIO/db/db_client_guilds.lua
+++ RaiderIO/db/db_client_guilds.lua
@@ -1,5 +1,5 @@
 --
--- RaiderIO Client Data. Generated on 2019-08-09T07:55:51Z
+-- RaiderIO Client Data. Generated on 2019-08-14T04:39:11Z
 --
 local _, ns = ...
 ns.GUILD_BEST_DATA = {
@@ -11,6 +11,108 @@ ns.GUILD_BEST_DATA = {
     },
     ["season_best"] = {
       [1] = {
+        ["zone_id"] = 9525,
+        ["level"] = 10,
+        ["upgrades"] = 0,
+        ["fraction"] = 1.8611348120328488,
+        ["clear_time"] = "01:18:11.919",
+        ["party"] = {
+          [1] = {
+            ["name"] = "Protecz",
+            ["role"] = "tank",
+            ["class_id"] = 1
+          },
+          [2] = {
+            ["name"] = "Chuckofdoom",
+            ["role"] = "healer",
+            ["class_id"] = 2
+          },
+          [3] = {
+            ["name"] = "Gomba",
+            ["role"] = "dps",
+            ["class_id"] = 6
+          },
+          [4] = {
+            ["name"] = "Tedrocalypse",
+            ["role"] = "dps",
+            ["class_id"] = 12
+          },
+          [5] = {
+            ["name"] = "Âêgon",
+            ["role"] = "dps",
+            ["class_id"] = 12
+          }
+        }
+      },
+      [2] = {
+        ["zone_id"] = 9525,
+        ["level"] = 9,
+        ["upgrades"] = 1,
+        ["fraction"] = 0.9945636630557966,
+        ["clear_time"] = "00:41:47.294",
+        ["party"] = {
+          [1] = {
+            ["name"] = "Deadork",
+            ["role"] = "tank",
+            ["class_id"] = 6
+          },
+          [2] = {
+            ["name"] = "Noobtagonist",
+            ["role"] = "healer",
+            ["class_id"] = 5
+          },
+          [3] = {
+            ["name"] = "Drakkenwolf",
+            ["role"] = "dps",
+            ["class_id"] = 2
+          },
+          [4] = {
+            ["name"] = "Protecz",
+            ["role"] = "dps",
+            ["class_id"] = 1
+          },
+          [5] = {
+            ["name"] = "Sindiirus",
+            ["role"] = "dps",
+            ["class_id"] = 5
+          }
+        }
+      },
+      [3] = {
+        ["zone_id"] = 9391,
+        ["level"] = 9,
+        ["upgrades"] = 0,
+        ["fraction"] = 1.027812230091989,
+        ["clear_time"] = "00:33:56.095",
+        ["party"] = {
+          [1] = {
+            ["name"] = "Lostiam",
+            ["role"] = "tank",
+            ["class_id"] = 11
+          },
+          [2] = {
+            ["name"] = "Oneofthree",
+            ["role"] = "healer",
+            ["class_id"] = 11
+          },
+          [3] = {
+            ["name"] = "Gomba",
+            ["role"] = "dps",
+            ["class_id"] = 6
+          },
+          [4] = {
+            ["name"] = "Gucciflirt",
+            ["role"] = "dps",
+            ["class_id"] = 8
+          },
+          [5] = {
+            ["name"] = "Protecz",
+            ["role"] = "dps",
+            ["class_id"] = 1
+          }
+        }
+      },
+      [4] = {
         ["zone_id"] = 9526,
         ["level"] = 9,
         ["upgrades"] = 0,
@@ -44,7 +146,7 @@ ns.GUILD_BEST_DATA = {
           }
         }
       },
-      [2] = {
+      [5] = {
         ["zone_id"] = 9164,
         ["level"] = 9,
         ["upgrades"] = 0,
@@ -77,281 +179,10 @@ ns.GUILD_BEST_DATA = {
             ["class_id"] = 8
           }
         }
-      },
-      [3] = {
-        ["zone_id"] = 9525,
-        ["level"] = 9,
-        ["upgrades"] = 0,
-        ["fraction"] = 1.1544356027114648,
-        ["clear_time"] = "00:48:30.331",
-        ["party"] = {
-          [1] = {
-            ["name"] = "Chuckofdoom",
-            ["role"] = "tank",
-            ["class_id"] = 2
-          },
-          [2] = {
-            ["name"] = "Sukmyudder",
-            ["role"] = "healer",
-            ["class_id"] = 11
-          },
-          [3] = {
-            ["name"] = "Gomba",
-            ["role"] = "dps",
-            ["class_id"] = 6
-          },
-          [4] = {
-            ["name"] = "Hurji",
-            ["role"] = "dps",
-            ["class_id"] = 8
-          },
-          [5] = {
-            ["name"] = "Protecz",
-            ["role"] = "dps",
-            ["class_id"] = 1
-          }
-        }
-      },
-      [4] = {
-        ["zone_id"] = 9525,
-        ["level"] = 8,
-        ["upgrades"] = 1,
-        ["fraction"] = 0.8154937784584603,
-        ["clear_time"] = "00:34:15.859",
-        ["party"] = {
-          [1] = {
-            ["name"] = "Fandrals",
-            ["role"] = "tank",
-            ["class_id"] = 11
-          },
-          [2] = {
-            ["name"] = "Norriana",
-            ["role"] = "healer",
-            ["class_id"] = 5
-          },
-          [3] = {
-            ["name"] = "Ccu",
-            ["role"] = "dps",
-            ["class_id"] = 3
-          },
-          [4] = {
-            ["name"] = "Darththrock",
-            ["role"] = "dps",
-            ["class_id"] = 8
-          },
-          [5] = {
-            ["name"] = "Protecz",
-            ["role"] = "dps",
-            ["class_id"] = 1
-          }
-        }
-      },
-      [5] = {
-        ["zone_id"] = 9164,
-        ["level"] = 8,
-        ["upgrades"] = 1,
-        ["fraction"] = 0.9312634685832754,
-        ["clear_time"] = "00:30:44.832",
-        ["party"] = {
-          [1] = {
-            ["name"] = "Fionnslight",
-            ["role"] = "tank",
-            ["class_id"] = 2
-          },
-          [2] = {
-            ["name"] = "Chuckofdoom",
-            ["role"] = "healer",
-            ["class_id"] = 2
-          },
-          [3] = {
-            ["name"] = "Jeffreyy",
-            ["role"] = "dps",
-            ["class_id"] = 7
-          },
-          [4] = {
-            ["name"] = "Protecz",
-            ["role"] = "dps",
-            ["class_id"] = 1
-          },
-          [5] = {
-            ["name"] = "Tedrocalypse",
-            ["role"] = "dps",
-            ["class_id"] = 12
-          }
-        }
       }
     },
     ["weekly_best"] = {
-      [1] = {
-        ["zone_id"] = 9526,
-        ["level"] = 9,
-        ["upgrades"] = 0,
-        ["fraction"] = 1.0937021395089803,
-        ["clear_time"] = "00:45:57.222",
-        ["party"] = {
-          [1] = {
-            ["name"] = "Yoram",
-            ["role"] = "tank",
-            ["class_id"] = 1
-          },
-          [2] = {
-            ["name"] = "Chuckofdoom",
-            ["role"] = "healer",
-            ["class_id"] = 2
-          },
-          [3] = {
-            ["name"] = "Protecz",
-            ["role"] = "dps",
-            ["class_id"] = 1
-          },
-          [4] = {
-            ["name"] = "Scyren",
-            ["role"] = "dps",
-            ["class_id"] = 7
-          },
-          [5] = {
-            ["name"] = "Tedrocalypse",
-            ["role"] = "dps",
-            ["class_id"] = 12
-          }
-        }
-      },
-      [2] = {
-        ["zone_id"] = 9525,
-        ["level"] = 8,
-        ["upgrades"] = 1,
-        ["fraction"] = 0.8154937784584603,
-        ["clear_time"] = "00:34:15.859",
-        ["party"] = {
-          [1] = {
-            ["name"] = "Fandrals",
-            ["role"] = "tank",
-            ["class_id"] = 11
-          },
-          [2] = {
-            ["name"] = "Norriana",
-            ["role"] = "healer",
-            ["class_id"] = 5
-          },
-          [3] = {
-            ["name"] = "Ccu",
-            ["role"] = "dps",
-            ["class_id"] = 3
-          },
-          [4] = {
-            ["name"] = "Darththrock",
-            ["role"] = "dps",
-            ["class_id"] = 8
-          },
-          [5] = {
-            ["name"] = "Protecz",
-            ["role"] = "dps",
-            ["class_id"] = 1
-          }
-        }
-      },
-      [3] = {
-        ["zone_id"] = 9164,
-        ["level"] = 8,
-        ["upgrades"] = 1,
-        ["fraction"] = 0.9312634685832754,
-        ["clear_time"] = "00:30:44.832",
-        ["party"] = {
-          [1] = {
-            ["name"] = "Fionnslight",
-            ["role"] = "tank",
-            ["class_id"] = 2
-          },
-          [2] = {
-            ["name"] = "Chuckofdoom",
-            ["role"] = "healer",
-            ["class_id"] = 2
-          },
-          [3] = {
-            ["name"] = "Jeffreyy",
-            ["role"] = "dps",
-            ["class_id"] = 7
-          },
-          [4] = {
-            ["name"] = "Protecz",
-            ["role"] = "dps",
-            ["class_id"] = 1
-          },
-          [5] = {
-            ["name"] = "Tedrocalypse",
-            ["role"] = "dps",
-            ["class_id"] = 12
-          }
-        }
-      },
-      [4] = {
-        ["zone_id"] = 9527,
-        ["level"] = 7,
-        ["upgrades"] = 1,
-        ["fraction"] = 0.9676862414096443,
-        ["clear_time"] = "00:34:51.169",
-        ["party"] = {
-          [1] = {
-            ["name"] = "Elsyf",
-            ["role"] = "tank",
-            ["class_id"] = 6
-          },
-          [2] = {
-            ["name"] = "Khanji",
-            ["role"] = "healer",
-            ["class_id"] = 11
-          },
-          [3] = {
-            ["name"] = "Krilic",
-            ["role"] = "dps",
-            ["class_id"] = 3
-          },
-          [4] = {
-            ["name"] = "Lceman",
-            ["role"] = "dps",
-            ["class_id"] = 2
-          },
-          [5] = {
-            ["name"] = "Protecz",
-            ["role"] = "dps",
-            ["class_id"] = 1
-          }
-        }
-      },
-      [5] = {
-        ["zone_id"] = 9164,
-        ["level"] = 6,
-        ["upgrades"] = 2,
-        ["fraction"] = 0.7221083907664769,
-        ["clear_time"] = "00:23:50.496",
-        ["party"] = {
-          [1] = {
-            ["name"] = "Hollydrizity",
-            ["role"] = "tank",
-            ["class_id"] = 2
-          },
-          [2] = {
-            ["name"] = "Reml",
-            ["role"] = "healer",
-            ["class_id"] = 7
-          },
-          [3] = {
-            ["name"] = "Protecz",
-            ["role"] = "dps",
-            ["class_id"] = 1
-          },
-          [4] = {
-            ["name"] = "Stanki",
-            ["role"] = "dps",
-            ["class_id"] = 12
-          },
-          [5] = {
-            ["name"] = "Zetsuei",
-            ["role"] = "dps",
-            ["class_id"] = 2
-          }
-        }
-      }
+
     }
   }
 }
